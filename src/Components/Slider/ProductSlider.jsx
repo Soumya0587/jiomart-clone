30de08b6d4ad691986f45e98fc199d91cb914331
Soumya0587 <ghoshalsoumalya96@gmail.com>
2023-04-18 11:43:18 +0530

    product slider added

---
+++ src/Components/Slider/ProductSlider.jsx
@@ -0,0 +1,105 @@
+import React from "react";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+
+import {
+  Card,
+  Image,
+  Stack,
+  Heading,
+  Text,
+  Button,
+  Flex,
+  Box,
+  useToast
+} from "@chakra-ui/react";
+ 
+import { Link } from "react-router-dom";
+
+const ProductSlider = ({ data,value }) => {
+ 
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 3000 },
+      items: 6,
+      slidesToSlide: 3,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 6,
+      slidesToSlide: 3,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 4,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 2,
+    },
+  };
+
+  return (
+    <div style={{ width: "95%", margin: "auto", marginTop: "10px" }}>
+      {/* <Flex w={"80%"} margin={"auto"} pt={3} justifyContent={"space-between"}> */}
+        <Heading size="sm">Top {value}</Heading>
+      {/* </Flex> */}
+      <div style={{ width: "95%", margin: "auto" }}>
+        <Carousel responsive={responsive}>
+          {data.map((item, index) => (
+            <Card maxW="sm" h="400px" key={index} mr={1} ml={1}>
+              <Flex alignItems={"center"} justifyContent={"center"}>
+              <Link to={`product/${item._id}`} ><Image
+                  
+                  
+                  w={"100px"}
+                  h={"150px"}
+                  display={"block"}
+                  src={item.image[0]}
+                  borderRadius="lg"
+                /></Link>
+              </Flex>
+              <Stack p="6" bg="white">
+                {/* <Flex gap={4} textAlign={"center"}>
+                  <Box bg={"#00B5B7"} color={"white"} pl={3} pr={3}>
+                    {item.rating} <span style={{ fontSize: "20px" }}>*</span>
+                  </Box>
+                  <Text fontSize={12}>{item.review} reviews</Text>
+                </Flex> */}
+                <Heading size="sm">
+                  {item.product_name}
+                </Heading>
+                <Flex gap={2} alignItems={"center"}>
+                  <Heading fontSize="xl">₹ {item.discounted_price}</Heading>{" "}
+                  <Text color="gray" textDecoration={"line-through"}>
+                    ₹{item.retail_price}
+                  </Text>
+                  <Text color="green.600" fontSize="14">
+                    {item.discount}
+                    % Off
+                  </Text>
+                </Flex>
+              </Stack>
+              <Button
+                onClick={() => handleAddtoCart(item)}
+                w={"95%"}
+                margin="auto"
+                mb="3"
+                boxShadow="lg"
+                p="6"
+                bg="white"
+                _hover={{ bg: "orange", color: "white" }}
+                colorScheme="orange"
+                variant="outline"
+              >
+                Add to cart
+              </Button>
+            </Card>
+          ))}
+        </Carousel>
+      </div>
+    </div>
+  );
+};
+
+export default ProductSlider;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,27 @@
-import React from "react";
+import {useState,useEffect} from "react";
 import Carousel from "../Components/HomeCarousel/Carousel";
 import "../styles/Home.css";
+import axios from "axios"
 import Slider1 from "../Components/Slider/Slider1";
 import Slider2 from "../Components/Slider/Slider2";
 import { PosterData,FoodAndBeveragesData } from "../Components/Slider/SliderData";
+import {BASE_URL} from "../Util/Constant.js"
+import ProductSlider from "../Components/Slider/ProductSlider";
 const Home = () => {
+  const [Beverages,SetBeverages]=useState([])
+  const [Chocolates,SetChocolates]=useState([])
+  const [Fruits,SetFruits]=useState([])
+  const [FruitJuices,SetFruitJuices]=useState([])
+
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}/grocery?category=Beverages`).then((res)=>SetBeverages(res.data))
+    axios.get(`${BASE_URL}/grocery?sub_category=Chocolates & Candies`).then((res)=>SetChocolates(res.data))
+    axios.get(`${BASE_URL}/grocery?category=Fruits & Vegetables`).then((res)=>SetFruits(res.data))
+    axios.get(`${BASE_URL}/grocery?sub_category=Fruit Juices`).then((res)=>SetFruitJuices(res.data))
+   
+  }, []);
+  console.log(Beverages);
   return (
     <div>
       <Carousel />
@@ -15,7 +32,10 @@ const Home = () => {
       />
       <Slider1 data={PosterData} />
       <Slider2 data={FoodAndBeveragesData} heading="Food & Beverages" />
-
+      <ProductSlider data={Beverages} value="Beverages"/>
+      <ProductSlider data={Chocolates} value="Chocolates"/>
+      <ProductSlider data={Fruits} value="Fruits & Vegetables"/>
+      <ProductSlider data={FruitJuices} value="Fruit Juices"/>
     </div>
   );
 };
